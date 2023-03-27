032e91b5b6598770f1289bec80f856259d2a3087
lambert34 <aleksandrlambert@gmail.com>
2023-03-27 15:38:00 +0700

    добавляет функцию с логикой конвертации

--- start/converter.js
+++ start/converter.js
@@ -2,3 +2,11 @@ const RATES = {
     usd: 0.013,
     eur: 0.12
 }
+
+function convert(rub, currency) {
+    if (!RATES[currency]) {
+        return null;
+    }
+
+    return rub * RATES[currency];
+}
