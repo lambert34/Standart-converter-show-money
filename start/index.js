35285b64c64f222f0bdc31d5d2ef9719d642e62b
lambert34 <aleksandrlambert@gmail.com>
2023-03-27 15:59:52 +0700

    реффнул код в новую функцию

--- start/index.js
+++ start/index.js
@@ -2,17 +2,20 @@ const valueInputNode = document.querySelector('.js-value-input');
 const currencySelectorNode = document.querySelector('.js-currency-selector');
 const outputNode = document.querySelector('.js-output');
 
+function getInput() {
+    return {
+        rub: Number(valueInputNode.value),
+        currency: currencySelectorNode.value;
+    }
+}
+
 valueInputNode.addEventListener('input', function() {
-    const rub = Number(valueInputNode.value);
-    const currency = currencySelectorNode.value;
     const result = convert(rub, currency);
 
     outputNode.innerText = result;
 });
 
 currencySelectorNode.addEventListener('change', function() {
-    const rub = Number(valueInputNode.value);
-    const currency = currencySelectorNode.value;
     const result = convert(rub, currency);
 
     outputNode.innerText = result;
