b72d86e842dd8b0f30d751a9b4f10bf4560a02d9
lambert34 <aleksandrlambert@gmail.com>
2023-03-27 15:45:42 +0700

    добавили вывод нужных элементов на сайт и в консоль

--- start/index.js
+++ start/index.js
@@ -1,3 +1,7 @@
 const valueInputNode = document.querySelector('.js-value-input');
 const currencySelectorNode = document.querySelector('.js-currency-selector');
 const outputNode = document.querySelector('.js-output');
+
+valueInputNode.addEventListener('input', function() {
+    console.log(valueInputNode.value);
+})
