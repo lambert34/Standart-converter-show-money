4c7b0407a3ae73d5a1847de8eaff336971d133bb
lambert34 <aleksandrlambert@gmail.com>
2023-03-27 15:42:12 +0700

    добавляет элементы для будущей работы

---
+++ start/index.js
@@ -0,0 +1,3 @@
+const valueInputNode = document.querySelector('.js-value-input');
+const currencySelectorNode = document.querySelector('.js-currency-selector');
+const outputNode = document.querySelector('.js-output');
